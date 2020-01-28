6168d5f369c12e16fc0bb938cf0bdea9c41c2ac5
uicdb <161106938@qq.com>
2020-01-28 16:27:33 +0800

    noon

---
+++ AndroidTest/app/src/main/java/com/uicdb/code/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uicdb.code;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
